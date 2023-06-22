b4dedb365dd20224f5dd3efe7121e3ce9f6642ba
tohidshabanloo <tshabanloo@hotmail.com>
2023-06-22 12:55:23 +0330

    added sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,20 @@
+const Sitemap = () => {};
+const toUrl = (route) =>
+  `<url><loc>${route.url}</loc><lastmod>${route.lastmod}</lastmod></url>`;
+
+const createSitemap = (urlList) =>
+  `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urlList.map((url) => toUrl(url)).join("")}
+    </urlset>`;
+
+export async function getServerSideProps({ res, req }) {
+  const siteMapJson = await fetch(`https://www.exampleapi.com/getsitemap`);
+  const urlList = await siteMapJson.json();
+  const sitemap = createSitemap(urlList);
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+  return { props: { results: { urlList } } };
+}
+export default Sitemap;
