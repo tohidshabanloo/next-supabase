3d281290426f15d9358e91a8ea3fc330845ae11e
tohidshabanloo <tshabanloo@hotmail.com>
2023-08-29 15:50:32 +0300

    change style of index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,7 +46,7 @@ export default function Home({ data }: { data: any }) {
           content="Woah! You made it to my personal website, welcome."
         />
       </Head>
-      <div className="max-w-2xl border-gray-200 dark:border-gray-700 mt-20 mb-20">
+      <div className="max-w-4xl border-gray-200 dark:border-gray-700 mt-20 mb-20">
         <div className="flex flex-col-reverse sm:flex-row items-start">
           <div className="flex flex-col pr-12">
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-white">
@@ -88,24 +88,45 @@ export default function Home({ data }: { data: any }) {
         <h3 className="font-bold text-3xl tracking-tight mb-4 text-white">
           مقاله های اخیر
         </h3>
-        <div className="grid gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 max-w-4xl mx-auto">
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 max-w-4xl mx-auto">
           {data.map((article: any) => (
             <div key={article.slug}>
               <Link
                 href={`/article/${article.slug}`}
-                className="mb-4 transform hover:scale-[1.05] h-full transition-all text-white bg-white/5 border border-zinc-800/50 rounded-lg flex flex-col overflow-hidden hover:ring-2 ring-gray-300"
+                className=" transform hover:scale-[1.05] h-full transition-all text-white bg-white/5 border border-zinc-800/50 rounded-lg flex flex-col overflow-hidden hover:ring-2 ring-gray-300"
               >
                 <div>
-                  <div className="px-4">
-                    <h1
-                      className="mt-4 text-lg font-semibold line-clamp-title"
-                      title={article.title}
-                    >
-                      {article.title}
-                    </h1>
-                    <div className="mt-2 flex justify-between text-sm">
-                      <div>{article.published_at}</div>
-                      <div>{article.views} بازدید</div>
+                  <div className="grid sm:grid-cols-2 md:grid-cols-2">
+                    <div className="">
+                      <Image
+                        alt="توحید شعبانلو"
+                        height={140}
+                        width={140}
+                        src={article.image}
+                        className={cn(
+                          "object-cover h-64 w-48 rounded-r-lg duration-700 ease-in-out   md:flex select-none",
+                          isLoading
+                            ? "grayscale blur-2xl scale-110"
+                            : "grayscale-0 blur-0 scale-100"
+                        )}
+                        onLoadingComplete={() => setLoading(false)}
+                        priority={true}
+                      />
+                    </div>
+                    <div>
+                      <h1
+                        className="mt-4 text-lg font-semibold line-clamp-title"
+                        title={article.title}
+                      >
+                        {article.title}
+                      </h1>
+                      <h1 className="mt-4 ml-4 text-sm font-semibold text-justify">
+                        {article.description}
+                      </h1>
+                      <div className="gap-4 mb-2 absolute bottom-0   flex justify-between text-sm">
+                        <div>{article.published_at}</div>
+                        <div>{article.views} بازدید</div>
+                      </div>
                     </div>
                   </div>
                 </div>
