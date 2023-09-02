67c0b1d69e27c606f01dc961879280c9294e5f15
tohidshabanloo <tshabanloo@hotmail.com>
2023-09-02 04:01:50 +0300

    clodinary and other things added to new article

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ const nextConfig = {
       "images.unsplash.com",
       "google.com",
       "ditty.ir",
+      "localhost",
     ],
   },
 };
--- pages/dashboard/new.tsx
+++ pages/dashboard/new.tsx
@@ -19,36 +19,65 @@ import {
 import { BsTypeStrikethrough } from "react-icons/bs";
 import { RiHeading } from "react-icons/ri";
 import supabase from "../../lib/supabase";
+import axios from "axios";
+import Image from "next/image";
 
 export default function Edit() {
   const session = useSession();
   const [saveChanges, setSaveChanges] = useState("پست");
+  const [avatarUrl, setAvatarUrl] = useState("profile?.avatar_url" || "");
+  const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("");
+  const [secureUrl, setImage] = useState("");
+
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+    setFilename(URL.createObjectURL(event.target.files[0]));
+  };
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    const title = e.target.elements.title.value;
-    const image = e.target.elements.image.value;
-    const tags = e.target.elements.tags.value;
-    const content = e.target.elements.content.value;
-    const slug = e.target.elements.slug.value;
-    const published = e.target.elements.published.value;
+    setLoading(true);
+    // router.push(`/article/${data.slug}`);
+    const formData = new FormData();
 
-    const { data, error } = await supabase
-      .from("blog")
-      .insert({ title, image, tags, content, slug, published });
+    formData.append("file", file);
+    formData.append("upload_preset", "supabase");
 
-    if (error) {
-      throw error;
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dufbokly6/image/upload",
+        formData
+      );
+
+      const title = e.target.elements.title.value;
+      const description = e.target.elements.description.value;
+      const image = response?.data?.secure_url;
+      const tags = e.target.elements.tags.value;
+      const content = e.target.elements.content.value;
+      const slug = e.target.elements.slug.value;
+      const published = e.target.elements.published.value;
+      const { data, error } = await supabase
+        .from("blog")
+        .insert({ title, description, image, tags, content, slug, published });
+
+      setLoading(false);
+      setSaveChanges("منتشر شد!");
+
+      // setTimeout(() => {
+      //   setSaveChanges("Publish");
+      // }, 5000);
+
+      // router.push(`/article/${slug}`);
+      setSaveChanges("ذخیره شد!");
+      // setTimeout(() => {
+      //   setSaveChanges("ذخیره");
+      // }, 5000);
+    } catch (error) {
+      console.log(error);
     }
-
-    setSaveChanges("منتشر شد!");
-
-    setTimeout(() => {
-      setSaveChanges("Publish");
-    }, 5000);
-
-    router.push(`/article/${slug}`);
   };
 
   const ref = useRef<TextareaMarkdownRef>(null);
@@ -81,17 +110,32 @@ export default function Edit() {
                 required
               />
             </label>
-
             <label className="font-bold text-sm mb-1">
-              عکس کاور<span className="text-red-500 mr-1">*</span>
+              توضیحات<span className="text-red-500 mr-1">*</span>
               <input
                 className="w-full p-2 bg-white/5 border border-zinc-800/50 text-sm mb-4 rounded-lg font-normal placeholder:text-[#888888]"
                 type="text"
-                name="image"
-                placeholder="your-image-here"
+                name="description"
+                placeholder="your-description-here"
+                required
               />
             </label>
-
+            <label className="font-bold text-sm mb-1">
+              عکس کاور<span className="text-red-500 mr-1">*</span>
+              <input
+                className="flex justify-between w-full p-2 bg-white/5 border border-zinc-800/50 text-sm mb-4 rounded-lg font-normal"
+                name="image"
+                type="file"
+                onChange={handleFileChange}
+                // required
+              />
+            </label>
+            {/* <Image
+              alt={`${data?.title}`}
+              width={400}
+              height={400}
+              src={`${filename ? filename : data?.image}`}
+            /> */}
             <label className="font-bold text-sm mb-1">
               اسلاگ<span className="text-red-500 mr-1">*</span>
               <input
@@ -223,7 +267,27 @@ export default function Edit() {
                 />
               </Fragment>
             </label>
-
+            {loading ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            ) : null}
             <button
               className="py-1 px-6 rounded-lg bg-green-600 flex items-center justify-center hover:ring-2 ring-gray-300 transition-all"
               title="Save Changes"
