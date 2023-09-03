af27f954428c118d9597fc81e1c35223efa291fa
tohidshabanloo <tshabanloo@hotmail.com>
2023-09-03 10:23:12 +0300

    filtered data.includes.tag "learn" and changed css

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,11 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ data }: { data: any }) {
+  //return only data with 'learn' tag
+  const leranData = data.filter((item: any) => item.tags.includes("js"));
+
   const [isLoading, setLoading] = useState(true);
-  console.log(data);
+  // console.log(data);
   return (
     <>
       <Head>
@@ -145,13 +148,73 @@ export default function Home({ data }: { data: any }) {
               <FiArrowLeft className="ml-1 h-4 w-4 my-auto" />
             </Link>
           </div>
+
           {/* <div>
             <h4 className="flex mt-4 text-[#888888] rounded-lg hover:text-white transition-all">
-              <Link href="/tracks">
-                <NowPlaying />
-              </Link>
+            <Link href="/tracks">
+            <NowPlaying />
+            </Link>
             </h4>
           </div> */}
+        </div>
+        <h3 className="font-bold text-3xl tracking-tight mb-4 mt-4 text-white">
+          جاوا اسکریپت
+        </h3>
+        <div className="grid grid-cols-3 gap-4  max-w-4xl mx-auto ">
+          {leranData.map((article: any) => (
+            <div key={article.slug}>
+              <Link
+                href={`/article/${article.slug}`}
+                className=" transform hover:scale-[1.05] h-full transition-all text-white bg-white/5 border border-zinc-800/50 rounded-lg flex flex-col overflow-hidden hover:ring-2 ring-gray-300"
+              >
+                <div>
+                  <div className="row sm:grid-cols-2 md:grid-cols-2 ">
+                    <div className="">
+                      <Image
+                        alt="فرانت لند"
+                        height={1000}
+                        width={1000}
+                        src={article.image}
+                        className={cn(
+                          "object-cover h-32 w-100 rounded-t-lg duration-700 ease-in-out   md:flex select-none",
+                          isLoading
+                            ? "grayscale blur-2xl scale-110"
+                            : "grayscale-0 blur-0 scale-100"
+                        )}
+                        onLoadingComplete={() => setLoading(false)}
+                        priority={true}
+                      />
+                    </div>
+                    <div className="mx-4 mb-10">
+                      <h1
+                        className="text-yellow-500 mt-4 text-lg font-semibold line-clamp-title"
+                        title={article.title}
+                      >
+                        {article.title}
+                      </h1>
+                      <h1 className="mt-4 ml-4 text-sm font-extralight text-gray-400  text-justify w-42  line-clamp-6">
+                        {article.description}
+                      </h1>
+                      <div className="gap-4 mb-2 absolute bottom-2  text-gray-400  flex justify-between text-sm">
+                        <div>{article.published_at}</div>
+                        <div>{article.views} بازدید</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Link
+            href="/blog"
+            title="Read all posts"
+            className="flex mt-4 text-[#888888] rounded-lg hover:text-white transition-all"
+          >
+            مشاهده تمامی پست ها
+            <FiArrowLeft className="ml-1 h-4 w-4 my-auto" />
+          </Link>
         </div>
       </div>
     </>
